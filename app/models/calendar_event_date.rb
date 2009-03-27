5cc5672b9ec8584935b62bec2541ed12ee227467
Angel N. Sciortino <contact@angeliccomputing.com>
2009-03-27 08:08:56 -0500

    added new associations for event_date and event_mod

--- app/models/calendar_event_date.rb
+++ app/models/calendar_event_date.rb
@@ -1,7 +1,8 @@
 class CalendarEventDate < ActiveRecord::Base
   belongs_to :event, :class_name => 'CalendarEvent', :foreign_key => 'calendar_event_id'
   belongs_to :date, :class_name => 'CalendarDate', :foreign_key => 'calendar_date_id'
+  belongs_to :mod, :class_name => 'CalendarEventMod', :foreign_key => 'calendar_event_mod_id'
 
-  named_scope :removed, :conditions => {:removed => true}
-  named_scope :not_removed, :conditions => {:removed => false}
+  named_scope :removed, :conditions => {:removed => true}, :order => 'date_value'
+  named_scope :not_removed, :conditions => {:removed => false}, :order => 'date_value'
 end
--- app/models/calendar_event_mod.rb
+++ app/models/calendar_event_mod.rb
@@ -1,4 +1,8 @@
 class CalendarEventMod < ActiveRecord::Base
   belongs_to :event, :class_name => 'CalendarEvent', :foreign_key => 'calendar_event_id'
   belongs_to :date, :class_name => 'CalendarDate', :foreign_key => 'calendar_date_id'
+  has_one :event_date, :class_name => 'CalendarEventDate', :foreign_key => 'calendar_event_mod_id'
+
+  validates_presence_of :calendar_event_id, :calendar_date_id
+  validates_uniqueness_of :calendar_event_id, :scope => :calendar_date_id
 end
